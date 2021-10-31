b9788d70846d161393bc06b204e01d756311f3ba
Amir Reza Dalir <dalirnet@gmail.com>
2021-10-31 21:00:01 +0330

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
-/** @type {import('@sveltejs/kit').Config} */
+import vercel from '@sveltejs/adapter-vercel';
+
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: vercel(),
 		target: '#svelte'
 	}
 };
